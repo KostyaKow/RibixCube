decbac3c5db4d0245e05047a400d041930171e57
Kostyantyn Kovalskyy <kostelkow@gmail.com>
2016-07-03 20:43:08 -0400

    Added colors and few other things.

--- Assets/Game.cs
+++ Assets/Game.cs
@@ -5,6 +5,133 @@ using System.Collections.Generic;
 public class Game : MonoBehaviour {
    public string defaultColor;
    private Dictionary<string, Color> colors;
+   private int counter;
+   public List<GameObject> cubes;
+   private float left_to_rotate;
+   public float rot_frame_angle;
+
+	// Update is called once per frame
+	void Update () {
+      counter++;
+      if (counter % 300 == 0 || counter == 1) {
+         left_to_rotate += 90;
+         //rotateStrip(counter % 3, true, true, 90);
+
+         //var t = cubes[1].transform;
+         //t.RotateAround(Vector3.zero, Vector3.up, 20 * Time.deltaTime);
+      }
+
+      if (left_to_rotate > 0) {
+         left_to_rotate -= rot_frame_angle;
+         rotateStrip(0, false, true, rot_frame_angle);
+      }
+	}
+
+   //List<GameObject> getFace(string s) {} //BFUDLR
+   //bool isDone() {}
+
+   //rotate row/column at index, vertically/horizontally, forward/backward
+   void rotateStrip(int index, bool vert, bool forward, float angle) {
+      //number of subCubes to rotate at a time: 9 (8 without subcube in middle)
+      List<GameObject> cubes_to_rotate = new List<GameObject>();
+
+      for (int x = 0; x < 3; x++) {
+         for (int y = 0; y < 3; y++) {
+            for (int z = 0; z < 3; z++) {
+               if ((vert && index == x) || (!vert && index == y))
+                  cubes_to_rotate.Add(cubes[x*3*3+y*3+z]);
+            }
+         }
+      }
+
+      float degrees = forward ? angle : -angle;
+
+      Transform centralCubeT = cubes[1*3*3 + 1*3 + 1].transform;
+      //move central square up to check if we got right now
+      //if (counter == 1) centralCubeT.transform.position += new Vector3(0, 3, 0);
+
+      Vector3 alignV = new Vector3(0, 0, 0); //(0.5f, 0.5f, 0.5f);
+      Vector3 rotatePoint = centralCubeT.position - alignV;
+
+      if (vert) {
+         for (int i = 0; i < cubes_to_rotate.Count; i++) {
+            var t = cubes_to_rotate[i].transform;
+            t.RotateAround(rotatePoint, Vector3.right, degrees);
+         }
+      } else {
+         for (int i = 0; i < cubes_to_rotate.Count; i++) {
+            var t = cubes_to_rotate[i].transform;
+            t.RotateAround(rotatePoint, Vector3.up, degrees);
+         }
+      }
+   }
+
+	/* (F)ront = green
+      (B)ack = yellow
+      (L)eft = red
+      (R)ight = orange
+      (U)p = white
+      (D)own = blue
+
+      Start  with green (F)
+   */
+	void Start() {
+      //defaultColor = "black"
+      initColors();
+      counter = 0;
+      left_to_rotate = 0.0f;
+
+      cubes = new List<GameObject>();
+
+      //int num_cubes = 3*3*3; //1 of them in middle which is never visible
+
+      for (int i = 0; i < 3; i++) { //(x, y, z) = (i, j, k)
+         for (int j = 0; j < 3; j++) {
+            for (int k = 0; k < 3; k++) {
+               var sub = makeSubCube();
+               sub.name += i.ToString() + "-" +
+                           j.ToString() + "-" +
+                           k.ToString();
+
+               var moveV = new Vector3(i+0.05f*i, j+0.05f*j, k+0.05f*k);
+               sub.transform.position += moveV;
+               cubes.Add(sub);
+               if (k == 0) {
+                  if (!setSubCubeSideColor(sub, "F", "green")) {
+                     Debug.Log("can't initialize cube"); return;
+                  }
+               }
+               if (k == 2) {
+                  if (!setSubCubeSideColor(sub, "B", "yellow")) {
+                     Debug.Log("can't initialize cube"); return;
+                  }
+               }
+               if (i == 2) {
+                  if (!setSubCubeSideColor(sub, "R", "orange")) {
+                     Debug.Log("can't initialize cube"); return;
+                  }
+               }
+               if (i == 0) {
+                  if (!setSubCubeSideColor(sub, "L", "red")) {
+                     Debug.Log("can't initialize cube"); return;
+                  }
+               }
+               if (j == 0) {
+                  if (!setSubCubeSideColor(sub, "D", "blue")) {
+                     Debug.Log("can't initialize cube"); return;
+                  }
+               }
+               if (j == 2) {
+                  if (!setSubCubeSideColor(sub, "U", "white")) {
+                     Debug.Log("can't initialize cube"); return;
+                  }
+               }
+
+            }
+         }
+      }
+
+	}
 
    GameObject makeQuadFace(string c_str = null, GameObject parent = null) {
       var fType = PrimitiveType.Quad;
@@ -14,7 +141,7 @@ public class Game : MonoBehaviour {
       if (c_str == null)
          c_str = defaultColor;
 
-      Debug.Log(c_str);
+      //Debug.Log(c_str);
       Color c = colors[c_str]; //new Color(0, 0, 0);
 
       Renderer rend = face.GetComponent<Renderer>();
@@ -36,7 +163,7 @@ public class Game : MonoBehaviour {
    {
 
       GameObject o = new GameObject(); //Parent Object
-      o.name = "SubCube Container";
+      o.name = "SubCubeContainer";
 
       var face_far = makeQuadFace(c1, o);
       face_far.name = "B";
@@ -70,56 +197,44 @@ public class Game : MonoBehaviour {
       frt.position += new Vector3(0.5f, 0, -0.5f);
       frt.eulerAngles += new Vector3(0, 90+180, 90);
 
+      setParent(o, gameObject);
       return o;
-   }
-
-	/* (F)ront = green
-      (B)ack = yellow
-      (L)eft = red
-      (R)ight = orange
-      (U)p = white
-      (D)own = blue
-
-      Start  with green (F)
-   */
-	void Start() {
-      //defaultColor = "black"
-      initColors();
-
-      List<GameObject> cube = new List<GameObject>();
-
-      int num_cubes = 3*3*3; //1 of them in middle which is never visible
-
-      for (int i = 0; i < 3; i++) {
-         for (int j = 0; j < 3; j++) {
-            for (int k = 0; k < 3; k++) {
-               var face = makeSubCube("black");
-               face.transform.position += new Vector3(i+0.1f*i, j+0.1f*j, k+0.1f*k);
-            }
-         }
-      }
-	}
-
-   Dictionary<string, string> getNeighbors(string color) {
-      return new Dictionary<string, string>();
-   }
-
-	// Update is called once per frame
-	void Update () {
-
-	}
-
-   void setParent(GameObject child, GameObject parent) {
-      child.transform.parent = parent.transform;
    }
 
    void initColors() {
       colors = new Dictionary<string, Color>();
+      colors.Add("yellow", new Color(1, 1, 0));
+      colors.Add("orange", new Color(1, 0.8f, 0));
       colors.Add("red", new Color(1, 0, 0));
       colors.Add("green", new Color(0, 1, 0));
       colors.Add("blue", new Color(0, 0, 1));
-      colors.Add("black", new Color(0, 0, 0));
       colors.Add("white", new Color(1, 1, 1));
+
+      colors.Add("black", new Color(0, 0, 0));
+   }
+
+   bool setSubCubeSideColor(GameObject o, string side_str, string color_str = null)
+   {
+      //var side = o.Find(side_str);
+      var side = GameObject.Find(o.name + "/" + side_str);
+      if (side == null) return false;
+      Debug.Log("got side");
+      if (color_str == null) color_str = "black";
+
+      var c = colors[color_str];
+      if (c == null) return false;
+
+      setColor(side, c);
+      return true;
+   }
+
+   void setColor(GameObject o, Color c) {
+      Renderer rend = o.GetComponent<Renderer>();
+      rend.material.color = c;
+   }
+
+   void setParent(GameObject child, GameObject parent) {
+      child.transform.parent = parent.transform;
    }
 
 }
